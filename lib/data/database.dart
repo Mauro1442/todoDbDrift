d60fef1bd7e699bfd3e1a0bbb3505643ecd052c3
Mauro1442 <maurodirosa19@gmail.com>
2023-04-19 18:53:46 +0200

    create database

---
+++ lib/data/database.dart
@@ -0,0 +1,25 @@
+import 'package:drift/drift.dart';
+
+part 'database.g.dart';
+
+class Tasks extends Table {
+  //autoIncrement automatically sets this to be the primary key
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+  DateTimeColumn get dueDate => dateTime().nullable()();
+  BoolColumn get completed => boolean().withDefault(const Constant(false))();
+}
+
+@DriftDatabase(tables: [Tasks])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase(QueryExecutor e) : super(e);
+
+  @override
+  int get schemaVersion => 1;
+
+  Future<List<Task>> getAllTasks() => select(tasks).get();
+  Stream<List<Task>> watchAllTasks() => select(tasks).watch();
+  Future insertTask(Task task) => into(tasks).insert(task);
+  Future updateTask(Task task) => update(tasks).replace(task);
+  Future deleteTask(Task task) => delete(tasks).delete(task);
+}
